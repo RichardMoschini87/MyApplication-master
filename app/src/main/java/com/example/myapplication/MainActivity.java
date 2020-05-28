1bb652ceb1f2f6678bf1a020faac1facb367421f
richardMoschini <moschini.richard@gmail.com>
2020-05-28 16:33:08 +0200

    Update MainActivity.java
    Prova

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     List<String> str = new ArrayList<String>();
     String e = "";
+    String u="";
 
 
     @Override
